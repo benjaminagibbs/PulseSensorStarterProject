d80eb6b9be7192cf31294aa33f1a03cea29bfc77
benjaminagibbs <bg@benjamingibbs.net>
2022-07-04 19:03:42 -0500

    gutted helpful material and did some code planning, kept pin ref image for now.

--- PulseSensor_StarterProject_Arduino/PulseSensor_StarterProject_Arduino.ino
+++ PulseSensor_StarterProject_Arduino/PulseSensor_StarterProject_Arduino.ino
@@ -1,25 +1,12 @@
-
-/*  PulseSensor™ Starter Project   http://www.pulsesensor.com
- *   
-This an Arduino project. It's Best Way to Get Started with your PulseSensor™ & Arduino. 
--------------------------------------------------------------
-1) This shows a live human Heartbeat Pulse. 
-2) Live visualization in Arduino's Cool "Serial Plotter".
-3) Blink an LED on each Heartbeat.
-4) This is the direct Pulse Sensor's Signal.  
-5) A great first-step in troubleshooting your circuit and connections. 
-6) "Human-readable" code that is newbie friendly." 
-
-*/
-
-
 //  Variables
 int PulseSensorPurplePin = 0;        // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
 int LED13 = 13;   //  The on-board Arduion LED
 
 
 int Signal;                // holds the incoming raw data. Signal value can range from 0-1024
-int Threshold = 550;            // Determine which Signal to "count as a beat", and which to ingore. 
+int Threshold_amp = 550;            // Determine which Signal to "count as a beat", and which to ingore. 
+int Threshold_freq;
+int event = 0;
 
 
 // The SetUp Function:
@@ -38,11 +25,13 @@ void loop() {
    Serial.println(Signal);                    // Send the Signal value to Serial Plotter.
 
    
-   if(Signal > Threshold){                          // If the signal is above "550", then "turn-on" Arduino's on-Board LED.  
+   if(Signal > Threshold_amp){                          // If the signal is above "550", then "turn-on" Arduino's on-Board LED.  
      digitalWrite(LED13,HIGH);          
    } else {
      digitalWrite(LED13,LOW);                //  Else, the sigal must be below "550", so "turn-off" this LED.
    }
+
+   // TODO: write frequency function, set event to active if frequency is above Threshold_freq
 
 
 delay(10);
